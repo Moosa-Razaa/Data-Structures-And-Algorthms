2ef82c81db9eb56b8073504a6e67cb7b3495b406
Moosa Raza <74827544+Moosa-Razaa@users.noreply.github.com>
2021-11-28 19:34:05 +0500

    C# Code For Linear Search
    A generic function to implement Linear Search that can work with all primitive data types.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    private static bool LinearSearch<T>( IEnumerable<T> array, T number) where T : IComparable
+    {
+        foreach(T item in array)
+        {
+            if (Equals(item , number) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void Main()
+    {
+    }
+}
