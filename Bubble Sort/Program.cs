54708076e7d03a16a420d7df8da5ca32f4b32ac3
Moosa Raza <74827544+Moosa-Razaa@users.noreply.github.com>
2021-11-29 02:03:01 +0500

    Bubble Sort in C#
    A generic class named Bubble Sort. This class will have implementation for all the data types that support +, -, / and *. 
    Simple implementation of bubble sort is also in the class you can check that as well if you want just basic bubble sort algorithm.
    A basic implementation of how to use this is also presented in program.cs file.

---
+++ Bubble Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using BubbleSort;
+
+int[] arr = { 9, 5, 0, 8, 1, 2, 6 };
+
+Bubble_Sort<int> bubble = new(arr);
+Console.WriteLine(bubble.GetCurrentType);
+
+var sorted = bubble.Sort();
+foreach(var item in sorted)
+{
+    Console.WriteLine(item);
+}
